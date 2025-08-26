a86319098257085b3baf485345c9c887df31b85d
kongsakchai <ksakchai.dev@gmail.com>
2025-08-26 13:59:34 +0700

    :wrench: add `trailingSlash` to `always` in view page

--- src/routes/view/[...file]/+page.server.ts
+++ src/routes/view/[...file]/+page.server.ts
@@ -6,9 +6,10 @@ import path from 'path'
 const contentPath = env.SLIDEMD_PATH || 'src/examples'
 
 export const prerender = true
+export const trailingSlash = 'always'
 
 export const load = async ({ params }) => {
-	const filePath = path.join(contentPath, params.file + '.md')
+	const filePath = path.join(contentPath, params.file.slice(0, params.file.length - 1) + '.md')
 	const resp = readFileSync(filePath, { encoding: 'utf-8' })
 
 	const markdown = resp
