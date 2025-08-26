adfbf924838968d661e104c444941db2f1ddfd17
kongsakchai <ksakchai.dev@gmail.com>
2025-08-26 22:14:02 +0700

    :wrench: update href attributes in file-item and folder-item components

--- src/routes/view/[...file]/+page.server.ts
+++ src/routes/view/[...file]/+page.server.ts
@@ -8,7 +8,11 @@ const contentPath = env.SLIDEMD_PATH || 'src/examples'
 export const trailingSlash = 'always'
 
 export const load = async ({ params }) => {
-	const filePath = path.join(contentPath, params.file)
+	let filePath = path.join(contentPath, params.file)
+	if (filePath.endsWith('/')) {
+		filePath = filePath.slice(0, filePath.length - 1)
+	}
+
 	const resp = readFileSync(filePath, { encoding: 'utf-8' })
 	return {
 		slide: await slide.process(resp)
