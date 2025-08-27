9d3be5c2f2419b904d1e89db5b3f62641330c2d0
kongsakchai <ksakchai.dev@gmail.com>
2025-08-27 17:35:04 +0700

    :wrench: update custom element

---
+++ src/examples/components/counter.svelte
@@ -0,0 +1,16 @@
+<svelte:options
+	customElement={{
+		tag: 'my-counter',
+		shadow: 'none'
+	}}
+/>
+
+<script lang="ts">
+	let data = $state(0)
+</script>
+
+<div class="flex flex-row items-center gap-3">
+	<button onclick={() => data--} class="border-line bg-primary h-fit w-12 rounded-sm border p-1"> - </button>
+	<p class="font-bold">{data}</p>
+	<button onclick={() => data++} class="border-line bg-primary h-fit w-12 rounded-sm border p-1"> + </button>
+</div>
--- src/examples/example.md
+++ src/examples/example.md
@@ -64,4 +64,8 @@ I just love **bold text**.
 Italicized text is the *cat's meow*.
 This text is ***really important***.
 
-<my-btn/>
+---
+
+## Custom Element
+
+<my-counter/>
